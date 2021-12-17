78996942ccb2200205c1ab967d10d338f65ff990
ronaldmirabal <ronaldmirabal@outlook.es>
2021-12-17 10:44:03 -0400

    Final

--- Proyecto-Finald.cpp
+++ Proyecto-Finald.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-
-
-
-
-int clientes(){
-	int x;
+	int clientes(){
+		int x;
+		
+		cout<<"-------MENU - CLIENTES -------"<<endl;
+		cout<<"1. Agregar Cliente"<<endl;
+		cout<<"2. Listar Cliente"<<endl;
+		cout<<"3. Salir"<<endl;
+		cout<<"Opcion:";
+		cin>>x;
+		return x;
+	}
 	
-	cout<<"-------MENU - CLIENTES -------"<<endl;
-	cout<<"1. Agregar Cliente"<<endl;
-	cout<<"2. Listar Cliente"<<endl;
-	cout<<"3. Salir"<<endl;
-	cout<<"Opcion:";
-	cin>>x;
-	return x;
-}
+	int Alquiler(){
+		int x;
+		cout<<"-------MENU - ALQUILER -------"<<endl;
+		cout<<"1. Agregar Alquiler"<<endl;
+		cout<<"2. Listar Alquileres"<<endl;
+		cout<<"3. Salir"<<endl;
+		cout<<"Opcion:";
+		cin>>x;
+		return x;
+	}
 	
 	void agregar(ofstream &es){
 		system("cls");
@@ -37,6 +44,62 @@ int clientes(){
 		cin>>codigoempresa;
 		es<<nombre<<" "<<cedula<<" "<<telefono<<" "<<direccion<<" "<<codigoempresa<<"\n";
 		es.close();
+	}
+		
+	void RegistrarAlquiler(ofstream &es, int codigo){
+		system("cls");
+		string pelicula;
+		string fecha;
+		double precio;
+		es.open("Alquiler.txt", ios::out | ios::app);
+		cout<<"Nombre de la pelicula:";
+		cin>>pelicula;
+		cout<<"Precio de la pelicula:";
+		cin>>precio;
+		cout<<"Fecha Entrega:";
+		cin>>fecha;
+		es<<pelicula<<" "<<precio<<" "<<fecha<<" "<<codigo<<"\n";
+		es.close();
+	}
+		
+	void AgregarAlquiler(ifstream &Lec){
+		system("cls");
+		ofstream Esc;
+		Lec.open("Clientes.txt", ios::in);
+		int codigoempresaaux;
+		string nombre;
+		string cedula;
+		string telefono;
+		string direccion;
+		int codigoempresa;
+		bool encontrado = false;
+		cout<<"Digite el numero de cliente: "<<endl;
+		cin>>codigoempresaaux;
+		Lec>>nombre;
+		while(!Lec.eof() && !encontrado){
+			Lec>>cedula;
+			Lec>>telefono;
+			Lec>>direccion;
+			Lec>>codigoempresa;
+			if(codigoempresa == codigoempresaaux){
+				cout<<"Nombre---:"<<nombre<<endl;
+				cout<<"Cedula---:"<<cedula<<endl;
+				cout<<"Telefono---:"<<telefono<<endl;
+				cout<<"Direccion---:"<<direccion<<endl;
+				cout<<"Codigo Empresa---:"<<codigoempresa<<endl;
+				cout<<"--------------------------"<<endl;
+				encontrado = true;
+			}
+			Lec>>nombre;
+		}
+		Lec.close();
+		if(!encontrado){
+			cout<<"Cliente no encontado"<<endl;
+			system("pause");
+		}else{
+			system("pause");
+			RegistrarAlquiler(Esc, codigoempresaaux);
+		}
 	}
 		
 	void VerCliente(ifstream &Lec){
@@ -64,6 +127,30 @@ int clientes(){
 		Lec.close();
 		system("pause");
 	}
+		
+	void ListarAlquiler(ifstream &Lec){
+		system("cls");
+		string pelicula;
+		string fecha;
+		double precio;
+		int codigo;
+		Lec.open("Alquiler.txt", ios::in);
+		Lec>>pelicula;
+		while(!Lec.eof()){
+			Lec>>precio;
+			Lec>>fecha;
+			Lec>>codigo;
+			
+			cout<<"Pelicula---:"<<pelicula<<endl;
+			cout<<"Precio---:"<<precio<<endl;
+			cout<<"Fecha Entrega---:"<<fecha<<endl;
+			cout<<"Codigo Cliente---:"<<codigo<<endl;
+			cout<<"--------------------------"<<endl;
+			Lec>>pelicula;
+		}
+		Lec.close();
+		system("pause");
+	}
 
 int main(int argc, char *argv[]) {
 	int opc;
@@ -73,8 +160,7 @@ int main(int argc, char *argv[]) {
 	cout<<"-------MENU-------"<<endl;
 	cout<<"1. Clientes"<<endl;
 	cout<<"2. Alquiler Video"<<endl;
-	cout<<"3. Reportes"<<endl;
-	cout<<"4. Salir"<<endl;
+	cout<<"3. Salir"<<endl;
 	cout<<"Opcion:";
 	cin>>opc;
 	
@@ -94,7 +180,23 @@ int main(int argc, char *argv[]) {
 			}
 		} while(op !=3);
 		break;
+	case 2:
+		int op2;
+		do{
+			system("cls");
+			op2 = Alquiler();
+			switch(op2){
+			case 1:
+				AgregarAlquiler(Lec);
+				break;
+			case 2:
+				ListarAlquiler(Lec);
+				break;
+			}
+		} while(op2 !=3);
+		break;
 	}
+	
 	return 0;
 }
 
